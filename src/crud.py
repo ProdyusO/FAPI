2b3bfe76d898fb317f66fe1869a092e73a028acd
Oleksii Prodyus <70231122+ProdyusO@users.noreply.github.com>
2021-10-21 10:15:25 +0300

    2
    second commit

--- src/crud.py
+++ src/crud.py
@@ -3,46 +3,47 @@ import model
 import schema
 
 
-def get_user_by_username(db: Session, username: str):
-    return db.query(model.User).filter(model.User.username == username).first()
+class CRUD:
+    def get_user_by_username(db: Session, username: str):
+        return db.query(model.User).filter(model.User.username == username).first()
 
 
-def get_user_by_id(db: Session, id: int):
-    return db.query(model.User).filter(model.User.id == id).first()
+    def get_user_by_id(db: Session, id: int):
+        return db.query(model.User).filter(model.User.id == id).first()
 
 
-def get_user(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(model.User).offset(skip).limit(limit).all()
+    def get_user(db: Session, skip: int = 0, limit: int = 100):
+        return db.query(model.User).offset(skip).limit(limit).all()
 
 
-def add_user_details_to_db(db: Session, user: schema.UserAdd):
-    details = model.User(
-        username=user.username,
-        email=user.email,
-        password=user.password,
-        register_date=user.register_date,
-    )
-    db.add(details)
-    db.commit()
-    db.refresh(details)
-    return model.User(**user.dict())
-
-
-def update_user_details(db: Session, id: int, details: schema.UpdateUser):
-    db.query(model.User).filter(model.User.id == id).update(vars(details))
-    db.commit()
-    return db.query(model.User).filter(model.User.id == id).first()
-
-
-def patch_user_details(db: Session, id: int, details: schema.PatchUser):
-    db.query(model.User).filter(model.User.id == id).update(vars(details))
-    db.commit()
-    return db.query(model.User).filter(model.User.id == id).first()
-
-
-def delete_user_details_by_id(db: Session, id: int):
-    try:
-        db.query(model.User).filter(model.User.id == id).delete()
+    def add_user_details_to_db(db: Session, user: schema.UserAdd):
+        details = model.User(
+            username=user.username,
+            email=user.email,
+            password=user.password,
+            register_date=user.register_date,
+        )
+        db.add(details)
         db.commit()
-    except Exception as e:
-        raise Exception(e)
+        db.refresh(details)
+        return model.User(**user.dict())
+
+
+    def update_user_details(db: Session, id: int, details: schema.UpdateUser):
+        db.query(model.User).filter(model.User.id == id).update(vars(details))
+        db.commit()
+        return db.query(model.User).filter(model.User.id == id).first()
+
+
+    def patch_user_details(db: Session, id: int, details: schema.PatchUser):
+        db.query(model.User).filter(model.User.id == id).update(vars(details))
+        db.commit()
+        return db.query(model.User).filter(model.User.id == id).first()
+
+
+    def delete_user_details_by_id(db: Session, id: int):
+        try:
+            db.query(model.User).filter(model.User.id == id).delete()
+            db.commit()
+        except Exception as e:
+            raise Exception(e)
--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 from fastapi.encoders import jsonable_encoder
 
-import crud
+from crud import CRUD
 import model
 import schema
 
@@ -25,43 +25,43 @@ def get_db():
 
 @app.post('/user/', response_model=schema.UserAdd)
 def add_new_user(user: schema.UserAdd, db: Session = Depends(get_db)):
-    username = jsonable_encoder(crud.get_user_by_username(db=db, username=user.username))
+    username = jsonable_encoder(CRUD.get_user_by_username(db=db, username=user.username))
     if username:
         raise HTTPException(status_code=400, detail=f"User id {user.username} already exist: {username}")
-    return jsonable_encoder(crud.add_user_details_to_db(db=db, user=user))
+    return jsonable_encoder(CRUD.add_user_details_to_db(db=db, user=user))
 
 
 @app.get('/user/{user_id}', response_model=schema.User)
 def get_user_by_id(id: int, db: Session = Depends(get_db)):
-    user = jsonable_encoder(crud.get_user_by_id(db=db, id=id))
+    user = jsonable_encoder(CRUD.get_user_by_id(db=db, id=id))
     return jsonable_encoder(user)
 
 
 @app.put('/user/{user_id}', response_model=schema.User)
 def update_user_details(id: int, update_param: schema.UpdateUser, db: Session = Depends(get_db)):
-    details = jsonable_encoder(crud.get_user_by_id(db=db, id=id))
+    details = jsonable_encoder(CRUD.get_user_by_id(db=db, id=id))
     if not details:
         raise HTTPException(status_code=404, detail=f"No record found to update")
-    return jsonable_encoder(crud.update_user_details(db=db, details=update_param, id=id))
+    return jsonable_encoder(CRUD.update_user_details(db=db, details=update_param, id=id))
 
 
 @app.patch('user/{user_id}', response_model=schema.User)
 def patch_user_details(id: int, update_param: schema.PatchUser, db: Session = Depends(get_db)):
-    details = jsonable_encoder(crud.get_user_by_id(db=db, id=id))
+    details = jsonable_encoder(CRUD.get_user_by_id(db=db, id=id))
     if not details:
         raise HTTPException(status_code=404, detail=f"No record found to update")
 
-    return jsonable_encoder(crud.patch_user_details(db=db, details=update_param, id=id))
+    return jsonable_encoder(CRUD.patch_user_details(db=db, details=update_param, id=id))
 
 
 @app.delete('/user/{user_id}')
 def delete_user_by_id(id: int, db: Session = Depends(get_db)):
-    details = jsonable_encoder(crud.get_user_by_id(db=db, id=id))
+    details = jsonable_encoder(CRUD.get_user_by_id(db=db, id=id))
     if not details:
         raise HTTPException(status_code=404, detail=f"Not found ")
 
     try:
-        jsonable_encoder(crud.delete_user_details_by_id(db=db, id=id))
+        jsonable_encoder(CRUD.delete_user_details_by_id(db=db, id=id))
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Unable to delete: {e}")
     return jsonable_encoder({"delete status": "success"})
@@ -69,5 +69,5 @@ def delete_user_by_id(id: int, db: Session = Depends(get_db)):
 
 @app.get('/user-list', response_model=List[schema.User])
 def get_all_user_details(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = jsonable_encoder(crud.get_user(db=db, skip=skip, limit=limit))
+    users = jsonable_encoder(CRUD.get_user(db=db, skip=skip, limit=limit))
     return users
