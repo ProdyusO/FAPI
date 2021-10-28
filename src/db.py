f2628763fe52963940c6cbd755375e68be002b2f
Oleksii Prodyus <70231122+ProdyusO@users.noreply.github.com>
2021-10-28 19:51:40 +0300

    Add Async
    Async

--- src/db.py
+++ src/db.py
@@ -1,13 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, MetaData
+import databases
+
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./user_database.db"
 
+database = databases.Database(SQLALCHEMY_DATABASE_URL)
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-Base = declarative_base()
+metadata = MetaData()
